4cdf1197a06d30d84dc1cc66040a621726ebf10c
Matt <mattmillergraphics@gmail.com>
2018-01-24 20:24:13 +0000

    BugFixes: Projectile Collision & Strafing

--- Source/EFPEAESS/EFPEAESSGameMode.cpp
+++ Source/EFPEAESS/EFPEAESSGameMode.cpp
@@ -9,7 +9,7 @@ AEFPEAESSGameMode::AEFPEAESSGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Character/Behaviour/Character.Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
